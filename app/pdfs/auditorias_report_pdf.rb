f52b9b64fbaffd1d96dc3204cf4871a8a2f2043d
Wenceslao Paez Chavez <wcpaez@gmail.com>
2014-10-11 11:09:01 -0500

    Base pdf report

--- app/pdfs/auditorias_report_pdf.rb
+++ app/pdfs/auditorias_report_pdf.rb
@@ -1,9 +1,25 @@
 class AuditoriasReportPdf < BasePdf
+  REPORT_NAME = 'Evaluación de evidencias'
   def initialize(tipo = "")
     super()
-    @tipo = 'hola'
-    header "AUDIT PERÚ-UC"
+    header
+    sub_header_left
+    sub_header_right
   end
   private
-  
+  def sub_header_left
+    super(REPORT_NAME)
+    text_box "AREA : #{REPORT_NAME}", :at => [@address_x, cursor]
+    move_down @lineheight_y
+    text_box "FICHA : #{current_datetime}", :at => [@address_x, cursor]
+    move_down @lineheight_y
+  end
+  def sub_header_right
+    report_header_data = [
+      ["Evidencias evaluadas",2],
+      ["Evidencias por evaluadar", 3],
+      ["TOTAL", 2]
+    ]
+    super(report_header_data)
+  end
 end
--- app/pdfs/base_pdf.rb
+++ app/pdfs/base_pdf.rb
@@ -3,17 +3,64 @@ class BasePdf < Prawn::Document
   
   def initialize(options={})
     super(options)
+    @address_x = 35
     @lineheight_y = 12
-    font_size 10
-    font 'Helvetica'
+    @font_size = 9
+    @report_header_x = 325
   end
   
-  def header(title=nil)
-    image LOGO_PATH, position: :left, height: 30
+  private
+  
+  def header
+    initial_y = cursor
+    initialmove_y = 5
+    move_down initialmove_y
+    change_font(type: 'Helvetica', size: @font_size)
+    render_header_text
+    render_header_logo
+  end
+
+
+  def change_font(**options)
+    font(options[:type] || 'Helvetica')
+    font_size(options[:size] || 10)
+  end
+  
+  def render_header_text
+    text_box "UNIVERSIDAD CONTINENTAL", at: [@address_x, cursor], style: :bold
     move_down @lineheight_y
-    text "Universidad Continental", size: 14, style: :bold, align: :center
-    if title
-      text title, size: 10, style: :bold_italic, align: :center
+    text_box "Programa Audit-UC", :at => [@address_x, cursor]
+    move_down @lineheight_y
+  end
+  
+  def render_header_logo
+    @last_measured_y = cursor
+    move_cursor_to bounds.height
+    image LOGO_PATH, :width => 200, :position => :right
+    move_cursor_to @last_measured_y
+  end
+  
+  def current_datetime
+    Time.now.strftime("%d/%m/%Y a las %I:%M%p")
+  end
+
+  def sub_header_left(report_name)
+    move_down 65
+    @last_measured_y = cursor
+    text_box "REPORTE : #{report_name}", :at => [@address_x, cursor]
+    move_down @lineheight_y
+    text_box "GENERADO : #{current_datetime}", :at => [@address_x, cursor]
+    move_down @lineheight_y
+  end
+
+  def sub_header_right(table_data)
+    move_cursor_to @last_measured_y  
+    table(table_data, :position => @report_header_x, :width => 215) do
+      style(row(0..1).columns(0..1), :padding => [1, 5, 1, 5], :borders => [])
+      style(row(2), :background_color => 'e9e9e9', :border_color => 'dddddd', :font_style => :bold)
+      style(column(1), :align => :right)
+      style(row(2).columns(0), :borders => [:top, :left, :bottom])
+      style(row(2).columns(1), :borders => [:top, :right, :bottom])
     end
   end
 end
