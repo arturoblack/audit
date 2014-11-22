597da8af5dac02f2e216cf06052c37b05e5a0792
Arturo Black <arturo.black0@gmail.com>
2014-11-21 23:48:53 -0500

    merge master 21/11

--- app/assets/javascripts/angular/services/area_service.js
+++ app/assets/javascripts/angular/services/area_service.js
@@ -13,24 +13,15 @@ app.factory('AreaService',
       auditorias: { method: 'GET',
         params: {areaId: '@areaId',resources: 'auditorias'}},
       create_auditoria: { method: 'POST',
-<<<<<<< HEAD
                 params: {areaId: '@areaId',resources: 'auditorias'}},
       indicadores: { method: 'GET',
                 params: { areaId: '@areaId',resources: 'auditorias'}},
       create_indicador: { method: 'POST',
                 params: { areaId: '@areaId',resources: 'auditorias'}},
-      update_indicador: { method: 'PUT',
-                params: { areaId: '@areaId',resources: 'auditorias'}},
-      remove_indicador: { method: 'DELETE',
-                params: { areaId: '@areaId',resources: 'auditorias'}},
-=======
-        params: {areaId: '@areaId',resources: 'auditorias'}},
       indicadores_gestion: { method: 'GET', 
         params: { areaId: 'areaId', resources: 'indicadores_gestion'}},
       indicadores_operativos: { mehtod: 'GET', 
         params: { areaId: '@areaId', resources: 'indicadores_operativos'}},  
-               
->>>>>>> 107af2e42eeb64d626893d498d3222a8f62f82bb
     }
   );
 }]);
